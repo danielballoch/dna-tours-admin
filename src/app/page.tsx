ff2df03bdcc8cf6589b86c84912a8313aaf4c3d4
Daniel Balloch <danielpbballoch@gmail.com>
2024-11-27 15:09:07 +1300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,69 @@
 import Image from "next/image";
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "Admin App | DNA New Zealand"
+}
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="bg-black text-white">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-6/12 mx-auto">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/dna-tours-logo.png"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+            <h1 className="text-5xl">Routing & Notifications App Demo</h1>
+            <form className="w-full mb-36">
+              <h2 className="text-xl my-4">Please Add Driver Details:</h2>
+              <div className="flex">
+                <div className="w-6/12">
+                  <label for="driver-name">Name:</label><br/>
+                  <input id="driver-name" name="driver-name" className="w-full text-black p-1" type="text"></input>
+                </div>
+                <div className=" w-6/12 ml-5">
+                  <label >Email:</label><br/>
+                  <input type="text" className="w-full text-black p-1" ></input>
+                </div>
+              </div>
+              <div className="py-5">
+                <label>Start Location: </label><br/>
+                <input type="text" className="w-full text-black p-1"></input>
+              </div>
+              <div className="py-5">
+                <label>End Location: </label><br/>
+                <input type="text" className="w-full text-black p-1"></input>
+              </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+              <h2 className="text-xl my-4 mt-10">Please Add Customer Details:</h2>
+              <div className="flex">
+                <div className="w-6/12">
+                  <label>Name:</label><br/>
+                  <input className="w-full text-black p-1" type="text"></input>
+                </div>
+                <div className="w-6/12 ml-5">
+                  <label >Email: </label><br/>
+                  <input className="w-full text-black p-1" type="text"></input>
+                </div>
+              </div>
+              <div className="py-5">
+                <label>Pickup Location: </label><br/>
+                <input className="w-full text-black p-1" type="text"></input>
+              </div>
+              <div className="flex">
+
+              <button className="w-7/12 p-5 mt-5 bg-yellow-500">Generate Route/Pickup Times</button>
+              <button className="w-4/12 p-5 mt-5 bg-sky-950 ml-auto">Add Customer</button>
+              </div>
+              
+            </form>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <a></a>
       </footer>
     </div>
   );
